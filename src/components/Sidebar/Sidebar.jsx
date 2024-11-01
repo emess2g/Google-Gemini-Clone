187dfc73a3c8f62d31982dfc8ffc202e7a4fe28f
emess2g <essuonemma84gmail.com>
2024-11-01 20:00:40 +0000

    recent prompt

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,9 +1,11 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import './Sidebar.css'
 import { assets } from '../../assets/assets'
+import { Context } from '../../context/Context';
 const Sidebar = () => {
 
     const [extended,setExtended] = useState(false);
+    const { onSent, prevPrompts, setRecentPrompt } = useContext(Context)
   return (
     <div className='sidebar'>
       <div className='top'>
@@ -15,10 +17,14 @@ const Sidebar = () => {
        {extended ?
           <div className="recent">
           <p className='recent-title'>Recent </p>
-          <div className="recent-entry">
+          {prevPrompts.map((item,index)=>{
+             return (
+              <div className="recent-entry">
               <img src={assets.message_icon} alt="" />
-              <p>What is react ... </p>
-          </div>
+              <p>{item.slice(0,18)} ... </p>
+              </div>
+             )
+          })}
          </div> : null
        }
       
